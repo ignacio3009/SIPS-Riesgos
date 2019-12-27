314dd69d55eace9d9018a9b4bb15e6209c2af08c
Ignacio Ñancupil <ignacio3009@hotmail.com>
2019-12-27 08:37:16 -0300

    Update 27 dic

--- SIPS/SCOPF_EVALUACION_TECNICA.py
+++ SIPS/SCOPF_EVALUACION_TECNICA.py
@@ -1,73 +1,5 @@
 import xlwings as xw
 import pandapower as pp
-import pandapower.plotting.simple_plot
-
-def evaluacionTec(Pgen,Qgen,Pcon,Qcon,PMAX,QMAX,PMIN,QMIN, filename, s, print_results=False):
-    # from pandapower.plotting.simple_plot import *
-    #create empty net
-    
-    net = pp.create_empty_network()
-    
-    #create buses    
-    pp.create_bus(net, index = 3, vn_kv=13.8, name="Bus GA",geodata=(1,1))
-    pp.create_bus(net, index = 4, vn_kv=13.8, name="Bus GB",geodata=(0,5))
-    pp.create_bus(net, index = 5, vn_kv=13.8, name="Bus GC",geodata=(2,5))
-    
-    pp.create_bus(net, index = 0, vn_kv=220, name="Bus A",geodata=(1,2))
-    pp.create_bus(net, index = 1, vn_kv=220, name="Bus B",geodata=(0,4))
-    pp.create_bus(net, index = 2, vn_kv=220, name="Bus C",geodata=(2,4))
-
-    #Create Generators
-    pp.create_gen(net,bus=0, p_mw = Pgen[0], sn_mva = 100, name="Gen1", in_service=(s!=1), 
-                    max_p_mw = PMAX[0], min_p_mw=PMIN[0], max_q_mvar=QMAX[0], min_q_mvar=QMIN[0], controllable=True, 
-                    slack=True)
-    
-    if(s==1):
-        pp.create_gen(net,bus=1, p_mw = Pgen[1], sn_mva = 100, name="Gen2", in_service=True, 
-                    max_p_mw = PMAX[1], min_p_mw=PMIN[1], max_q_mvar=QMAX[1], min_q_mvar=QMIN[1], controllable=True,
-                    slack=True)
-    else:
-        pp.create_gen(net,bus=1, p_mw = Pgen[1], sn_mva = 100, name="Gen2", in_service=True, 
-                    max_p_mw = PMAX[1], min_p_mw=PMIN[1], max_q_mvar=QMAX[1], min_q_mvar=QMIN[1], controllable=True, 
-                    slack=False)
-    
-    
-    pp.create_gen(net,bus=2, p_mw = Pgen[2], sn_mva = 100, name="Gen3", in_service=(s!=3), 
-                    max_p_mw = PMAX[2], min_p_mw=PMIN[2], max_q_mvar=QMAX[2], min_q_mvar=QMIN[2], controllable=True)
-    
-    #create branch elements
-    pp.create_transformer(net, hv_bus=0, lv_bus=3, std_type="250 MVA 220/13.8 kV", name="TrafoA")
-    pp.create_transformer(net, hv_bus=1, lv_bus=4, std_type="250 MVA 220/13.8 kV", name="TrafoB")
-    pp.create_transformer(net, hv_bus=2, lv_bus=5, std_type="250 MVA 220/13.8 kV", name="TrafoC")
-    
-    pp.create_line(net, from_bus=0, to_bus=1, length_km=100, std_type="490-AL1/64-ST1A 220.0", name="Line1",in_service=(s!=4))
-    pp.create_line(net, from_bus=1, to_bus=2, length_km=100, std_type="490-AL1/64-ST1A 220.0", name="Line2",in_service=(s!=5))
-    pp.create_line(net, from_bus=0, to_bus=2, length_km=100, std_type="490-AL1/64-ST1A 220.0", name="Line3",in_service=(s!=6))
-    
-    #Create Loads
-    pp.create_load(net, bus=0, p_mw = Pcon[0], q_mvar=Qcon[0], name= "LoadA")
-    pp.create_load(net, bus=1, p_mw = Pcon[1], q_mvar=Qcon[1], name= "LoadB")
-    pp.create_load(net, bus=2, p_mw = Pcon[2], q_mvar=Qcon[2], name= "LoadC")
-    
-    pp.runpp(net)
-    if(print_results):
-        print('----- Buses Results -----')
-        print(net.res_bus)
-        print('----- Lines Results -----')
-        print(net.res_line)
-        print('----- Transformers Results -----')
-        print(net.res_trafo)
-        print('----- Generators Results -----')
-        print(net.res_gen)
-    
-    row = [3,13,23,33,43,53,63]
-    wb = xw.Book(filename)
-    sht = wb.sheets['Tecnicos']
-    sht.range('B'+str(row[s])).value = net.res_bus
-    sht.range('I'+str(row[s])).value = net.res_line
-    sht.range('Z'+str(row[s])).value = net.res_trafo
-    sht.range('AP'+str(row[s])).value = net.res_gen
-    return 
 
 def clearContents(filename):
     wb = xw.Book(filename)
@@ -82,41 +14,128 @@ def clearContents(filename):
         
         
         
+def evalTec(NUMNODES,PGEN,GENDATA,DEMDATA,LINDATA,BATDATA,slack_gen1,slack_gen2, filename_res,s, print_results=False):
+    
+    numgen = len(GENDATA)
+    numlin = len(LINDATA)
+    numdem = len(DEMDATA)
+    # =========================================================================
+    # CREATE EMPTY NET
+    # =========================================================================
+    net = pp.create_empty_network(f_hz=50.0, sn_mva=100)
+
+    # =========================================================================
+    # CREATE BUSES
+    # =========================================================================
+    for i in range(numgen):
+        pp.create_bus(net,vn_kv=220, index = i, max_vm_pu=1.1, min_vm_pu=0.9)
+        
+    cntidx = NUMNODES
+    j=-1
+    
+    
+    for i in range(numgen):
+        barcon = GENDATA[i][1]
+        if(barcon>j):
+            j=j+1
+            pp.create_bus(net, index = cntidx+j, vn_kv=13.8, max_vm_pu=1.03, min_vm_pu=0.97)
+            
+    # =========================================================================
+    # CREATE GENERATORS
+    # =========================================================================    
+    j=-1
+    for i in range(numgen):
+        pp.create_gen(net, bus=GENDATA[i][1], p_mw = PGEN[i], sn_mva = GENDATA[i][11], in_service=(s!=(i+1)),
+                      max_p_mw = GENDATA[i][7], min_p_mw = GENDATA[i][8],max_q_mvar=GENDATA[i][9], min_q_mvar=GENDATA[i][10], controllable=False, 
+                      slack=False)
+        #create trafos     
+        barcon = GENDATA[i][1]
+        if(barcon>j):
+            j=j+1
+        pp.create_transformer(net, hv_bus=GENDATA[i][1], lv_bus=cntidx+j, std_type="250 MVA 220/13.8 kV")
+    
+    
+    #set slack gen
+    if(s==1):
+        net.gen['slack'][slack_gen2]=True
+    else:
+        net.gen['slack'][slack_gen1]=True
+        
+    # =========================================================================
+    # CREATE LINES
+    # =========================================================================
+    for i in range(numlin):
+        fmax = LINDATA[i][1]/(3**0.5*220)
+        ltype = {'typ'+str(i):{"r_ohm_per_km": LINDATA[i][5], "x_ohm_per_km": LINDATA[i][4], "c_nf_per_km": 10, "max_i_ka": fmax, "type": "ol", "qmm2":490, "alpha":4.03e-3}}
+        pp.create_std_types(net,data=ltype,element='line')
+        
+        pp.create_line(net, from_bus=LINDATA[i][2], to_bus=LINDATA[i][3], length_km=LINDATA[i][6], std_type="typ"+str(i), in_service=(s!=numgen+1+i))
+        
+    # =========================================================================
+    # CREATE LOADS
+    # =========================================================================
+    #create loads
+    for i in range(numdem):
+        pp.create_load(net,  bus=DEMDATA[i][0], p_mw = DEMDATA[i][1], q_mvar=DEMDATA[i][2])
+
+    # =========================================================================
+    # PRINT
+    # =========================================================================
+
+    pp.runpp(net)
+    if(print_results):
+        print('----- Buses Results -----')
+        print(net.res_bus)
+        print('----- Lines Results -----')
+        print(net.res_line)
+        print('----- Transformers Results -----')
+        print(net.res_trafo)
+        print('----- Generators Results -----')
+        print(net.res_gen)
+    
+    # =========================================================================
+    # SAVE
+    # =========================================================================
+    row = [3,13,23,33,43,53,63]
+    wb = xw.Book(filename_res)
+    sht = wb.sheets['Tecnicos']
+    sht.range('B'+str(row[s])).value = net.res_bus
+    sht.range('I'+str(row[s])).value = net.res_line
+    sht.range('Z'+str(row[s])).value = net.res_trafo
+    sht.range('AP'+str(row[s])).value = net.res_gen
+    return 
+
+    
+
+        
         
 # =============================================================================
-# EXAMPLE
+# TAKE RESULTS AND SIMULATE
 # =============================================================================
-# import numpy as np
-def solveAll():
-    filename = 'RES_CORRECTIVO.xlsx'
-    clearContents(filename)
+def solveAll(filename_data,filename_res):
+    clearContents(filename_res)
     for s in range(0,7):
         print('Simulating Scenario '+str(s)+'. Please wait...')
         letter = ['B','C','D','E','F','G','H']
-        PMIN =  [0,30,10]
-        PMAX =  [100,150,150]
-        # FMAX =  [50,100,100]
-        # CV   =  [5,30,120]
-        # CR   =  [10,10,10]
-        D    =  [30,60,120]
-        # RAMPMAX = [50,50,50]
-        # PROBSCEN = [0,0.01,0.01,0.01,0.01,0.01, 0.01]
-        # CRUP = [100,20,10]
-        # CRDOWN = [80,50,10]
+
+        wbd = xw.Book(filename_data)
+        sd = wbd.sheets['data']
         
-    
-        Qgen = [0,0,0]
-        Qcon=  [0,0,0]
-        QMAX = [100,100,100]
-        QMIN = [0,0,0]
+        GENDATA = sd.range('A4:L6').value
+        DEMDATA = sd.range('N4:P6').value
+        LINDATA = sd.range('R4:X6').value
+        BATDATA = sd.range('Z4:AK6').value
+        NUMNODES = 3
         
-        wb = xw.Book(filename)
+        wb = xw.Book(filename_res)
         sht = wb.sheets['Economicos']
-        psol = sht.range(letter[s]+'2:'+letter[s]+'4').value
-        wb = xw.Book(filename)
-        evaluacionTec(psol,Qgen,D,Qcon,PMAX,QMAX,PMIN,QMIN,filename,s,print_results=False)
-        wb.save()
-    
+        PGEN = sht.range(letter[s]+'2:'+letter[s]+'4').value
+        slack_gen1 = 0
+        slack_gen2 = 1
+        evalTec(NUMNODES,PGEN,GENDATA,DEMDATA,LINDATA,BATDATA,slack_gen1,slack_gen2, filename_res,s, print_results=False)
+
     print('End Simulations!')
     
-solveAll()
+filename_res = 'res_reservas.xlsx'
+filename_data = 'data_3_bus.xlsx'
+solveAll(filename_data,filename_res)
